335a2d90572aa4f32a4194d90c6704f08a30e115
Lindor <lindoh.maphalala@icloud.com>
2026-09-09 19:00:19 +0200

    Add cart item removal test

--- tests/ui/shopping.spec.js
+++ tests/ui/shopping.spec.js
@@ -16,6 +16,16 @@ test('adding an item updates the cart badge', async ({ page }) => {
     await expect(inventoryPage.cartBadge).toHaveText('1');
 });
 
+test('removing an item empties the cart', async ({ page }) => {
+    const inventoryPage = new InventoryPage(page);
+
+    await inventoryPage.addItemToCart('sauce-labs-backpack');
+    await expect(inventoryPage.cartBadge).toHaveText('1');
+
+    await inventoryPage.removeItemFromCart('sauce-labs-backpack');
+    await expect(inventoryPage.cartBadge).toHaveCount(0);
+});
+
 test('user can complete checkout end to end', async ({ page }) => {
     const inventoryPage = new InventoryPage(page);
     const cartPage = new CartPage(page);
